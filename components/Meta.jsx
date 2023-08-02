ac38f2b8ee4014cca8e32c84a0a13cd127ff835a
n-ii-ma <88039431+n-ii-ma@users.noreply.github.com>
2023-08-02 12:25:28 +0330

    Add meta tag for robots

--- components/Meta.jsx
+++ components/Meta.jsx
@@ -5,6 +5,7 @@ const Meta = ({ keywords, description, title }) => (
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <meta name="keywords" content={keywords} />
     <meta name="description" content={description} />
+    <meta name="robots" content="all" />
     <meta charSet="utf-8" />
     <link rel="icon" href="/favicon.ico" />
     <title>{title}</title>
