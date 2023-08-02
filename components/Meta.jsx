f0ac72c6820e712fa870e8c3e6f8b59ee3543568
n-ii-ma <88039431+n-ii-ma@users.noreply.github.com>
2023-08-02 11:08:22 +0330

    Create Meta component to handle `head`

---
+++ components/Meta.jsx
@@ -0,0 +1,21 @@
+import Head from "next/head";
+
+const Meta = ({ keywords, description, title }) => (
+  <Head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta name="keywords" content={keywords} />
+    <meta name="description" content={description} />
+    <meta charSet="utf-8" />
+    <link rel="icon" href="/favicon.ico" />
+    <title>{title}</title>
+    meta
+  </Head>
+);
+
+Meta.defaultProps = {
+  title: "iOT Expo",
+  keywords: "internet of things, iot",
+  description: "Explore Everything About iOT",
+};
+
+export default Meta;
