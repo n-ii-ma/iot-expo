ecb9cf2e9f795ecff0056fbe3bc666b18d176606
n-ii-ma <88039431+n-ii-ma@users.noreply.github.com>
2023-08-05 15:39:38 +0330

    Turn title into link

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 import Nav from "./Nav";
 
 import headerStyles from "@/styles/Header.module.css";
@@ -5,9 +7,11 @@ import headerStyles from "@/styles/Header.module.css";
 const Header = () => {
   return (
     <header className={headerStyles.container}>
-      <h1 className={headerStyles.title}>
-        IoT <span>Expo</span>
-      </h1>
+      <Link href="/">
+        <h1 className={headerStyles.title}>
+          IoT <span id="span">Expo</span>
+        </h1>
+      </Link>
       <Nav />
     </header>
   );
