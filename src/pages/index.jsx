cd6ce8494d2bcf938bbdfb093bb7972c82d54a2f
n-ii-ma <88039431+n-ii-ma@users.noreply.github.com>
2023-08-08 13:31:49 +0330

    Add First Service component and its styles and assets

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,10 +1,12 @@
 import Banner from "../../components/Banner";
+import FirstService from "../../components/FirstService";
 import Newsletter from "../../components/Newsletter";
 
 export const Home = () => {
   return (
     <div>
       <Banner />
+      <FirstService />
       <Newsletter />
     </div>
   );
