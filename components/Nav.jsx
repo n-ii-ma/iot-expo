8d350aee66fc9bc8fcd2756f62cd12cb84842685
n-ii-ma <88039431+n-ii-ma@users.noreply.github.com>
2023-08-12 10:07:21 +0330

    Handle active link styling

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -1,21 +1,44 @@
 import Link from "next/link";
+import { useRouter } from "next/router";
 
 import navStyles from "@/styles/Nav.module.css";
 
-const Nav = () => (
-  <nav className={navStyles.container}>
-    <ul>
-      <li>
-        <Link href="/#services">Services</Link>
-      </li>
-      <li>
-        <Link href="/about-us">Company</Link>
-      </li>
-      <li>
-        <Link href="/pricing">Pricing</Link>
-      </li>
-    </ul>
-  </nav>
-);
+const Nav = () => {
+  const { asPath } = useRouter();
+
+  return (
+    <nav className={navStyles.container}>
+      <ul>
+        <li
+          className={
+            asPath === "/#services"
+              ? navStyles.activeLink
+              : navStyles.inactiveLink
+          }
+        >
+          <Link href="/#services">Services</Link>
+        </li>
+        <li
+          className={
+            asPath === "/about-us"
+              ? navStyles.activeLink
+              : navStyles.inactiveLink
+          }
+        >
+          <Link href="/about-us">Company</Link>
+        </li>
+        <li
+          className={
+            asPath === "/pricing"
+              ? navStyles.activeLink
+              : navStyles.inactiveLink
+          }
+        >
+          <Link href="/pricing">Pricing</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
 
 export default Nav;
