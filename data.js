13bda3c147d265c40655696f40bff615c1e9ab80
n-ii-ma <88039431+n-ii-ma@users.noreply.github.com>
2023-08-13 13:37:34 +0330

    Add dummy data

---
+++ data.js
@@ -0,0 +1,46 @@
+const prices = [
+  {
+    id: 1,
+    title: "Starter",
+    price: "$99",
+    purpose: "For developers or personal projects",
+    features: [
+      "10 Monthly Projects",
+      "Community Support",
+      "Advanced Analytics",
+      "Extended Support",
+      "250GB Storage",
+    ],
+  },
+  {
+    id: 2,
+    title: "Pro",
+    price: "$149",
+    purpose: "For startups or small projects",
+    features: [
+      "100 Monthly Projects",
+      "Community Support",
+      "Advanced Analytics",
+      "Extended Support",
+      "Extended Features",
+      "500GB Storage",
+    ],
+  },
+  {
+    id: 3,
+    title: "Ultimate",
+    price: "$199",
+    purpose: "For large or enterprise projects",
+    features: [
+      "150 Monthly Projects",
+      "Community Support",
+      "Advanced Analytics",
+      "Extended Support",
+      "Extended Features",
+      "Daily Backups",
+      "Unlimited Storage",
+    ],
+  },
+];
+
+export default prices;
