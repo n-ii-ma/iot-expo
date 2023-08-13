51fe63d5a1da1c6a97df3d0bb70780b26f9b5f47
n-ii-ma <88039431+n-ii-ma@users.noreply.github.com>
2023-08-13 14:35:47 +0330

    Return 404 page if data fetching fails

--- src/pages/pricing/index.jsx
+++ src/pages/pricing/index.jsx
@@ -11,36 +11,37 @@ const Pricing = ({ prices }) => (
     <section className={pricingStyles.container}>
       <Image src="/pricing.jpg" fill alt="Smart city at night" />
       <section className={pricingStyles.priceBox}>
-        {prices.map((plan) => (
-          <article key={plan.id}>
-            <div>
-              <div
-                className={
-                  plan.title === "Pro"
-                    ? pricingStyles.header
-                    : `${pricingStyles.header} ${pricingStyles.commonHeader}`
-                }
-              >
-                <h3>{plan.title}</h3>
-                <h4>{plan.price}</h4>
-                <p>per month</p>
+        {prices?.length &&
+          prices.map((plan) => (
+            <article key={plan.id}>
+              <div>
+                <div
+                  className={
+                    plan.title === "Pro"
+                      ? pricingStyles.header
+                      : `${pricingStyles.header} ${pricingStyles.commonHeader}`
+                  }
+                >
+                  <h3>{plan.title}</h3>
+                  <h4>{plan.price}</h4>
+                  <p>per month</p>
+                </div>
+                <div className={pricingStyles.body}>
+                  <h5>{plan.purpose}</h5>
+                  <ul>
+                    {plan.features.map((feature, index) => (
+                      <li key={index}>
+                        <p>{feature}</p>
+                      </li>
+                    ))}
+                  </ul>
+                </div>
               </div>
-              <div className={pricingStyles.body}>
-                <h5>{plan.purpose}</h5>
-                <ul>
-                  {plan.features.map((feature, index) => (
-                    <li key={index}>
-                      <p>{feature}</p>
-                    </li>
-                  ))}
-                </ul>
+              <div className={pricingStyles.footer}>
+                <button>Choose Plan</button>
               </div>
-            </div>
-            <div className={pricingStyles.footer}>
-              <button>Choose Plan</button>
-            </div>
-          </article>
-        ))}
+            </article>
+          ))}
       </section>
     </section>
   </>
@@ -55,7 +56,12 @@ export const getStaticProps = async () => {
         prices: response.data,
       },
     };
-  } catch (error) {}
+  } catch (error) {
+    // Return 404 page
+    return {
+      notFound: true,
+    };
+  }
 };
 
 export default Pricing;
