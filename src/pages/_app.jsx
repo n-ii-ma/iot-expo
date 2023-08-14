2f469cac2ee32d14b6e7aa7e1c101edac4f4a2db
n-ii-ma <88039431+n-ii-ma@users.noreply.github.com>
2023-08-14 16:27:04 +0330

    Integrade Vercel Web Analytics

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,4 +1,5 @@
 import Layout from "../../components/Layout";
+import { Analytics } from "@vercel/analytics/react";
 
 import "@/styles/globals.css";
 
@@ -6,6 +7,7 @@ export default function App({ Component, pageProps }) {
   return (
     <Layout>
       <Component {...pageProps} />
+      <Analytics />
     </Layout>
   );
 }
